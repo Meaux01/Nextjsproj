c1818ba9070a3e552d4c4d1d353175130d408f41
Meaux01 <maurice.grimes21@gmail.com>
2023-05-07 16:35:02 -0400

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function FirstPost(){
+    return (
+    <>
+    
+    <h1>First Post</h1>
+
+
+    
+    <h2><Link href='/'>Home Page</Link></h2>
+    
+    </>
+    )
+}
